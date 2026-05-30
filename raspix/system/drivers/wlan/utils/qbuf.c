b57d630ba825ca29e8d8c0eb71a0dc69056c1a51
misazhu <misa.zhu@gmail.com>
2026-05-31 00:54:21 +0800

    raspix wlan cpu busy fixed

--- raspix/system/drivers/wlan/utils/qbuf.c
+++ raspix/system/drivers/wlan/utils/qbuf.c
@@ -36,13 +36,13 @@ int queue_buffer_push(queue_buffer_t *qbuf, uint8_t* buf, int size){
 	if(qbuf->push_idx - qbuf->pop_idx < qbuf->qsize){
 		int idx = qbuf->push_idx % qbuf->qsize;
 		dst = &qbuf->bufs[idx];
-		qbuf->push_idx++;
 	}
 	
 	if(dst){
 		size = min(qbuf->bsize, size);
 		memcpy(dst->data, buf, size);
 		dst->size = size;
+		qbuf->push_idx++;
 		return size;
 	}
 	return 0;
@@ -53,16 +53,16 @@ int queue_buffer_pop(queue_buffer_t *qbuf, uint8_t* buf, int size){
 	if(qbuf->push_idx > qbuf->pop_idx){
 		int idx = qbuf->pop_idx % qbuf->qsize;
  		src = &qbuf->bufs[idx];
-		qbuf->pop_idx++;
-	}
-	if(qbuf->push_idx >= qbuf->qsize && qbuf->pop_idx >= qbuf->qsize){
-		qbuf->push_idx -= qbuf->qsize;
-		qbuf->pop_idx -= qbuf->qsize;
 	}
 
 	if(src){
 		size = min(src->size, size);
 		memcpy(buf, src->data, size);
+		qbuf->pop_idx++;
+		if(qbuf->push_idx >= qbuf->qsize && qbuf->pop_idx >= qbuf->qsize){
+			qbuf->push_idx -= qbuf->qsize;
+			qbuf->pop_idx -= qbuf->qsize;
+		}
 		return size;
 	}
 	return 0;
