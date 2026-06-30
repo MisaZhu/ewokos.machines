2ce7645c2ae02b2c7a46506407a4756b5c947e6c
misazhu <misa.zhu@gmail.com>
2026-06-30 15:56:16 +0800

    xgo.pi lcd driver updated

--- raspix/3rd/xgo.pi/drivers/spilcdd/spilcdd.c
+++ raspix/3rd/xgo.pi/drivers/spilcdd/spilcdd.c
@@ -5,13 +5,13 @@
 #include <string.h>
 #include <fb/fb.h>
 #include <ewoksys/vdevice.h>
-#include <arch/bcm283x/spi.h>
 #include <fbd/fbd.h>
-#include "lcd_reg.h"
+#include <st77xx/st77xx.h>
 
 static uint32_t flush(const fbinfo_t* fbinfo, const graph_t* g) {
+	(void)fbinfo;
 	uint32_t sz = 4 * g->w * g->h;
-	lcd_flush(g->buffer, sz);
+	st77xx_flush(g->buffer, sz);
 	return sz;
 }
 
@@ -36,19 +36,25 @@ static int32_t init(uint32_t w, uint32_t h, uint32_t dep) {
   */
 int main(int argc, char** argv) {
 	int lcd_rst = 27;
-	int lcd_rs = 25;
+	int lcd_dc = 25;
+	int lcd_cs = 8;
 	int lcd_bl = 0;
 
 	const char* mnt_point = argc > 1 ? argv[1]: "/dev/fb0";
 
 	if(argc > 4) {
 		lcd_rst = atoi(argv[2]);
-		lcd_rs = atoi(argv[3]);
+		lcd_dc = atoi(argv[3]);
 		lcd_bl = atoi(argv[4]);
 	}
 
-	bcm283x_spi_init();
-	lcd_init(lcd_rst, lcd_rs, lcd_bl, 4);
+	/*
+	 * The original local driver writes MADCTL=0x70 while flushing, which
+	 * matches the st77xx shared driver in LCD_MODE_0 + G_ROTATE_90.
+	 */
+	LCD_MODE = LCD_MODE_0;
+	st77xx_init(LCD_WIDTH, LCD_HEIGHT, G_ROTATE_90, 1,
+			lcd_dc, lcd_cs, lcd_rst, lcd_bl > 0 ? lcd_bl : -1, 4);
 
 	fbd_t fbd;
 	fbd.splash = NULL;
