87756ca71218ac678b2b2fb9c1268d7c7f77f7c9
misazhu <misa.zhu@gmail.com>
2026-07-08 22:13:51 +0800

    raspix i2ctouch driver

--- raspix/3rd/waveshare.pi/i2c.touch/drivers/gt911_touchd/gt911_touchd.c
+++ raspix/3rd/waveshare.pi/i2c.touch/drivers/gt911_touchd/gt911_touchd.c
@@ -7,10 +7,17 @@
 #include <ewoksys/mmio.h>
 #include "gt911/gt911.h"
 
+#define TP_POLL_MIN_US       8000u   /* ~125Hz while touching */
+#define TP_POLL_MAX_US       50000u  /* back off to 20Hz when idle */
+#define TP_RELEASE_DELAY_MS  20
+
 static bool press = false;
 static	TouchCordinate_t cordinate[5];
 static 	uint8_t  number_of_cordinate = 0;
-static 	uint64_t last_ts = 0;	
+static 	uint64_t last_ts = 0;
+static	uint16_t touch_data[3];
+static	bool     has_data = false;
+static	uint32_t poll_sleep_us = TP_POLL_MIN_US;
 
 static int tp_read(vdevice_t* dev, int fd, int from_pid, fsinfo_t* node,
 		void* buf, int size, int offset, void* p) {
@@ -20,31 +27,64 @@ static int tp_read(vdevice_t* dev, int fd, int from_pid, fsinfo_t* node,
 	(void)node;
 	(void)offset;
 	(void)p;
-	(void)size;
 
-	uint16_t* d = (uint16_t*)buf;
+	if (!has_data)
+		return VFS_ERR_RETRY;
+	if (size < 6)
+		return -1;
+
+	memcpy(buf, touch_data, 6);
+	has_data = false;
+	return 6;
+}
+
+static uint32_t tp_check_poll_events(vdevice_t* dev, int fd, int from_pid,
+		fsinfo_t* node, void* p) {
+	(void)dev;
+	(void)fd;
+	(void)from_pid;
+	(void)node;
+	(void)p;
+
+	return has_data ? VFS_EVT_RD : 0;
+}
+
+static int tp_loop(vdevice_t* dev, void* p) {
+	(void)p;
+	bool event = false;
+
 	number_of_cordinate = 0;
 	GT911_Status_t ret = GT911_ReadTouch(cordinate, &number_of_cordinate);
-	if (ret != GT911_OK || !number_of_cordinate)
-	{
-		if (press && (kernel_tic_ms(0) - last_ts) > 20)
-		{
+	if (ret != GT911_OK || !number_of_cordinate) {
+		if (press && (kernel_tic_ms(0) - last_ts) > TP_RELEASE_DELAY_MS) {
 			press = false;
-			d[0] = press;
-			d[1] = cordinate[0].x;
-			d[2] = cordinate[0].y;
-			return 6;
-		}else{
-			return 0;
+			touch_data[0] = press;
+			touch_data[1] = cordinate[0].x;
+			touch_data[2] = cordinate[0].y;
+			has_data = true;
+			event = true;
 		}
+	} else {
+		last_ts = kernel_tic_ms(0);
+		press = true;
+		touch_data[0] = press;
+		touch_data[1] = cordinate[0].x;
+		touch_data[2] = cordinate[0].y;
+		has_data = true;
+		event = true;
 	}
-	last_ts = kernel_tic_ms(0);
-	press = true;
-	d[0] = press;
-	d[1] = cordinate[0].x;
-	d[2] = cordinate[0].y;	
 
-	return 6;	
+	if (event) {
+		poll_sleep_us = TP_POLL_MIN_US;
+		vfs_wakeup(dev->mnt_info.node, VFS_EVT_RD);
+	} else if (poll_sleep_us < TP_POLL_MAX_US) {
+		poll_sleep_us <<= 1;
+		if (poll_sleep_us > TP_POLL_MAX_US)
+			poll_sleep_us = TP_POLL_MAX_US;
+	}
+
+	usleep(poll_sleep_us);
+	return 0;
 }
 
 int main(int argc, char** argv) {
@@ -57,6 +97,8 @@ int main(int argc, char** argv) {
 	memset(&dev, 0, sizeof(vdevice_t));
 	strcpy(dev.name, "gt911");
 	dev.read = tp_read;
+	dev.loop_step = tp_loop;
+	dev.check_poll_events = tp_check_poll_events;
 
 	device_run(&dev, mnt_point, FS_TYPE_CHAR, 0444);
 	return 0;
