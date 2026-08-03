e6966703daeddf1176a426c6ee03e94e970b5a6e
misazhu <misa.zhu@gmail.com>
2026-08-03 22:31:29 +0800

    hid_touchd speed up

--- raspix/system/drivers/hid_touchd/hid_touchd.c
+++ raspix/system/drivers/hid_touchd/hid_touchd.c
@@ -11,12 +11,30 @@
 
 #define HID_TOUCH_REPORT_ID 3
 #define HID_IDLE_SLEEP_MIN_US 1000u
-#define HID_IDLE_SLEEP_MAX_US 50000u
+#define HID_IDLE_SLEEP_MAX_US 4000u
+#define TOUCH_CACHE_SIZE 128u
 
 static int hid = -1;
-static uint16_t touch_data[3];
-static bool has_data = false;
+static uint16_t touch_data[TOUCH_CACHE_SIZE][3];
+static uint32_t touch_data_read = 0;
+static uint32_t touch_data_write = 0;
 static uint32_t idle_sleep_us = HID_IDLE_SLEEP_MIN_US;
+
+static bool touch_has_data(void) {
+        return (touch_data_write - touch_data_read) > 0;
+}
+
+static void touch_push(uint16_t state, uint16_t x, uint16_t y) {
+        if (touch_data_write - touch_data_read >= TOUCH_CACHE_SIZE) {
+                touch_data_read++;
+        }
+
+        uint16_t* evt = touch_data[touch_data_write % TOUCH_CACHE_SIZE];
+        evt[0] = state;
+        evt[1] = x;
+        evt[2] = y;
+        touch_data_write++;
+}
 
 static int touch_read(vdevice_t* dev, int fd, int from_pid, fsinfo_t* node,
 		void* buf, int size, int offset, void* p) {
@@ -27,15 +45,16 @@ static int touch_read(vdevice_t* dev, int fd, int from_pid, fsinfo_t* node,
 	(void)offset;
 	(void)p;
 
-	if (!has_data) {
+        if (!touch_has_data()) {
 		return VFS_ERR_RETRY;
 	}
 	if (size < 6) {
 		return -1;
 	}
 
-	memcpy(buf, touch_data, 6);
-	has_data = false;
+        memcpy(buf, touch_data[touch_data_read % TOUCH_CACHE_SIZE], 6);
+        memset(touch_data[touch_data_read % TOUCH_CACHE_SIZE], 0, sizeof(touch_data[0]));
+        touch_data_read++;
 	return 6;
 }
 
@@ -54,12 +73,12 @@ static uint32_t touch_check_poll_events(vdevice_t* dev, int fd, int from_pid, fs
 	(void)node;
 	(void)p;
 
-	return has_data ? VFS_EVT_RD : 0;
+        return touch_has_data() ? VFS_EVT_RD : 0;
 }
 
 static int touch_loop(vdevice_t* dev, void* p) {
 	uint8_t buf[8] = {0};
-	bool wakeup = false;
+        bool got = false;
 	(void)p;
 
 	ipc_disable();
@@ -68,18 +87,20 @@ static int touch_loop(vdevice_t* dev, void* p) {
 		if (ret != 7) {
 			break;
 		}
-		touch_data[0] = (uint16_t)buf[0];
-		touch_data[1] = (uint16_t)buf[1] | ((uint16_t)buf[2] << 8);
-		touch_data[2] = (uint16_t)buf[3] | ((uint16_t)buf[4] << 8);
-		has_data = true;
-		wakeup = true;
+                touch_push((uint16_t)buf[0],
+                                (uint16_t)buf[1] | ((uint16_t)buf[2] << 8),
+                                (uint16_t)buf[3] | ((uint16_t)buf[4] << 8));
+                got = true;
 	}
 	ipc_enable();
 
-	if (wakeup) {
+        if (touch_has_data()) {
 		idle_sleep_us = HID_IDLE_SLEEP_MIN_US;
 		vfs_wakeup(dev->mnt_info.node, VFS_EVT_RD);
 	}
+        else if (got) {
+                idle_sleep_us = HID_IDLE_SLEEP_MIN_US;
+        }
 	else {
 		usleep(idle_sleep_us);
 		if (idle_sleep_us < HID_IDLE_SLEEP_MAX_US) {
@@ -96,12 +117,21 @@ int main(int argc, char** argv) {
 	const char* mnt_point = argc > 1 ? argv[1] : "/dev/touch0";
 	const char* dev_point = argc > 2 ? argv[2] : "/dev/hid0";
 
-	hid = open(dev_point, O_RDONLY | O_NONBLOCK);
-	if (hid < 0) {
-		return -1;
-	}
-	if (set_report_id(hid, HID_TOUCH_REPORT_ID) != 0) {
-		return -1;
+        /*
+         * usbhostd may not have enumerated and registered the touch interface
+         * yet when hid_touchd starts. Keep retrying so /dev/touch0 does not
+         * disappear permanently after an early boot race.
+         */
+        while (1) {
+                hid = open(dev_point, O_RDONLY | O_NONBLOCK);
+                if (hid >= 0 && set_report_id(hid, HID_TOUCH_REPORT_ID) == 0) {
+                        break;
+                }
+                if (hid >= 0) {
+                        close(hid);
+                        hid = -1;
+                }
+                proc_usleep(200000);
 	}
 
 	vdevice_t dev;
