4693bafa50b335d7a219ccfc5e941e8b75b2deaf
misazhu <misa.zhu@gmail.com>
2026-08-06 08:17:34 +0800

    64bits address access

--- raspix/kernel/bsp/hw_info_arch.c
+++ raspix/kernel/bsp/hw_info_arch.c
@@ -139,7 +139,7 @@ void sys_info_init_arch(void) {
 	strcpy(_sys_info.arch, "armv7");
 #endif
 
-	_sys_info.mmio.size = _sys_info.total_phy_mem_size - _sys_info.mmio.phy_base;
+	_sys_info.mmio.size = 16*MB;
 	_sys_info.allocable_phy_mem_top = _sys_info.phy_offset +
 		_sys_info.total_usable_mem_size - PHY_RESV_SIZE;
 
