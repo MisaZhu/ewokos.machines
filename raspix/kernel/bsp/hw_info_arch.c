989329d780c9f552f188c59b2c0593f497c573d4
misazhu <misa.zhu@gmail.com>
2026-08-09 02:11:21 +0800

    raspix 8g ram

--- raspix/kernel/bsp/hw_info_arch.c
+++ raspix/kernel/bsp/hw_info_arch.c
@@ -80,6 +80,13 @@ void sys_info_init_arch(void) {
 		_core_base_offset =  0x01800000;
 		_pi4 = 1;
 	}
+	else if(pix_revision == PI_CM4_8G) {
+		strcpy(_sys_info.machine, "raspberry-cm4-8G");
+		_sys_info.total_phy_mem_size = 8ull*GB;
+		_sys_info.mmio.phy_base = 0xfe000000;
+		_core_base_offset =  0x01800000;
+		_pi4 = 1;
+	}
 	else if(pix_revision == PI_CM3_1G) {
 		strcpy(_sys_info.machine, "raspberry-cm3-1G");
 		_sys_info.total_phy_mem_size = 1u*GB;
